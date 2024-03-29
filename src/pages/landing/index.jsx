2208c8416fb57e06fa6d6886259e287f20838645
Gulzar Ahmad <gulzar.ahmad@conovoinc.com>
2024-03-30 00:20:31 +0500

    styling issue fixed and signup field added

--- src/pages/landing/index.jsx
+++ src/pages/landing/index.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './index.css'
 import Header from '../../components/header'
-import { Link } from 'react-router-dom'
 import Footer from '../../components/footer'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 const Landing = () => {
+  const [email, setEmail] = useState(null)
+
+  function validateEmail(email) {
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return re.test(String(email).toLowerCase());
+}
+
+  const handleSubmit = () =>{
+    if (validateEmail(email)) {
+      toast.success("Your have been signed up for beta launch!");
+      setEmail("")
+  } else {
+      toast.error("Email is not valid.");
+  }
+  }
   return (
     <div className='landing_page'>
         <Header/>
@@ -11,11 +27,18 @@ const Landing = () => {
             <h2 className='about_heading'>Node Deployment</h2>
             {/* <h3 className='about_subheading'>One click node deployment</h3> */}
             <p>
-            Noderr simplifies the world of blockchain, making nodes accessible to everyone. With effortless deployment, you can unlock exclusive early adopter rewards through our cutting-edge decentralization platform. Join us in shaping the future of blockchain accessibility
+            Noderr simplifies the world of blockchain, making nodes accessible to everyone. With effortless deployment, you can unlock exclusive early adopter rewards through our cutting-edge decentralized platform. Join us in shaping the future of blockchain accessibility
             .
             </p>
-            <Link to="/dashboard" className='btn primary'>Get started</Link>
+            <div className="input_container">
+            <label htmlFor="email">sign up for Beta launch</label>
+            <input type="email" id="email" placeholder='abc@xyz.com'value={email} onChange={(e)=>{setEmail(e.target.value)}}/>
+          </div>           
+          <button type="button" className="submit_btn" onClick={()=>{handleSubmit()}}>
+            Sign-Up
+          </button>
         </div>
+        <ToastContainer />
         <Footer/>
     </div>
   )
