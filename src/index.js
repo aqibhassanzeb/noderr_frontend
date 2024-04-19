8b4ff0a196a1ea808f45f6794226e6890ba7fa02
saudkhantc <saudkhantc@gmail.com>
2024-04-19 10:08:14 +0500

    change the app id in the index.js

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 const queryClient = new QueryClient()
 
-const projectId = process.env.REACT_APP_CONNECT_WALLET_ID
+const projectId = process.env.REACT_APP_CONNECT_WALLET_ID || "45fef58f3f6ccb3d8afa7b2883387d3c"
 
 const metadata = {
   name: 'Web3Modal',
