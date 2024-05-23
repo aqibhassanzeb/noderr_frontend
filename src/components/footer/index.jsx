9b81cd393d2212069cd2af0b8fb9b7ef3c8874b9
saudkhan <saudkhantc@gmail.com>
2024-05-23 14:22:36 +0500

    dashboard social icons and button fixes in different components

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import "./index.css";
 import { FaXTwitter } from "react-icons/fa6";
 import { FaDiscord, FaTelegramPlane } from "react-icons/fa";
+import {  BsInstagram } from "react-icons/bs";
+import { BsDiscord  } from "react-icons/bs";
 const Footer = () => {
   return (
     <div className="footer">
@@ -11,16 +13,7 @@ const Footer = () => {
       <span className="text">
         <a href="terms-and-conditions">Terms of Use</a>
       </span>
-      <span className="icon">
-        <a
-          href="https://discord.gg/SaqBPJn3ZA"
-          target="_blank"
-          aria-label="Discord"
-        >
-          <FaDiscord />
-        </a>
-      </span>
-      <span className="icon">
+      {/* <span className="icon">
         <a
           href="https://x.com/NoderrNoderrxyz?t=ur-xp14gkfRZiduGgzIqVA&s=09"
           target="_blank"
@@ -30,10 +23,35 @@ const Footer = () => {
         </a>
       </span>
       <span className="icon">
-        <a href="https://t.me/NoderrSupp" target="_blank" aria-label="Telegram">
-          <FaTelegramPlane />
+        <a
+          href="https://discord.gg/SaqBPJn3ZA"
+          target="_blank"
+          aria-label="Discord"
+        >
+          <BsDiscord />
         </a>
       </span>
+      <span className="icon">
+        <a
+          href="https://x.com/NoderrNoderrxyz?t=ur-xp14gkfRZiduGgzIqVA&s=09"
+          target="_blank"
+          aria-label="Twitter"
+        >
+          <FaTelegramPlane/>
+        </a>
+      </span>
+      <span className="icon">
+        <a href="https://t.me/NoderrSupp" target="_blank" aria-label="Telegram">
+          <BsInstagram />
+        </a>
+      </span> */}
+    <div id="w-node-_22aaa6c3-d6e7-05e0-eec1-5d107e13b5d4-ee31794f" className="fn-links-wrapper-2 flex items-center gap-2 p-1 pr-2">
+							<a href="https://twitter.com/NoderrNoderrxyz?t=ur-xp14gkfRZiduGgzIqVA&s=09" target="_blank" className="w-inline-block"><img src={require("./../../assets/images/2-p-500.png")} loading="lazy" style={{ width: "38px" }} alt srcSet={require("./../../assets/images/2-p-500.png")} className="image-14" /></a>
+
+							<a href="https://discord.com/invite/SaqBPJn3ZA" target="_blank" className="w-inline-block"><img src={require("./../../assets/images/3670325.png")} loading="lazy" style={{ width: "38px" }} alt srcSet={require("./../../assets/images/3670325.png")} className="image-14" /></a>
+							<a href="https://t.me/NoderrSupp" target="_blank" className="w-inline-block"><img style={{ width: "38px" }} src={require("./../../assets/images/telegram-black-icon.webp")} loading="lazy" alt className="image-13" /></a>
+							<a href="https://instagram.com/Noderrxyz" target="_blank" className="w-inline-block"><img src={require("./../../assets/images/1.png")} loading="lazy" style={{ width: "38px" }} alt className="image-14" /></a>
+						</div>
     </div>
   );
 };
--- src/pages/admin/nodes/createNode/index.jsx
+++ src/pages/admin/nodes/createNode/index.jsx
@@ -89,7 +89,7 @@ const CreateNode = () => {
       {loading && <LoadingModal />}
       <div className="right_dashboard">
         <div className="right_container">
-          <div className="">
+          <div className="mb-2">
             <span className="close" onClick={handleCloseCreate}>
               <IoArrowBackCircle className="text-white w-8 h-8" />
             </span>
